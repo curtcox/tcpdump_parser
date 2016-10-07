fb0d724a6d5240737d04d47515077e749424c0f7
Curt Cox <curtcox@gmail.com>
2016-10-06 20:59:37 -0500

    Parse invalid IP as null

--- src/IP.java
+++ src/IP.java
@@ -1,6 +1,22 @@
 final class IP {
 
     static IP parse(String[] parts) {
+        if (!isValid(parts)) {
+            return null;
+        }
         return new IP();
+    }
+
+    static boolean isValid(String[] parts) {
+        boolean ipFound = false;
+        for (String part : parts) {
+            if (ipFound && part.equals(">")) {
+                return true;
+            }
+            if (part.startsWith("IP")) {
+                ipFound = true;
+            }
+        }
+        return false;
     }
 }
