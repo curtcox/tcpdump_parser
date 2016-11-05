052f99e898281cec50ba6ae77d402545d2c7755d
Curt Cox <curtcox@gmail.com>
2016-11-05 06:49:14 -0500

    Added tests

--- src/DefaultGapDetector.java
+++ src/DefaultGapDetector.java
@@ -1,6 +1,8 @@
 final class DefaultGapDetector implements GapDetector {
     @Override
     public boolean isGapBetween(Timestamp t1, Timestamp t2) {
-        return t1 == null || t2 == null;
+        return t1 == null ||
+               t2 == null ||
+               t1. ;
     }
 }
---
+++ src/DefaultGapDetectorTest.java
@@ -0,0 +1,41 @@
+import org.junit.*;
+
+import static org.junit.Assert.assertFalse;
+
+public class DefaultGapDetectorTest {
+
+    DefaultGapDetector detector = new DefaultGapDetector();
+
+    @Test
+    public void no_gap() {
+        assertNoGap(Timestamp.MAX,  Timestamp.MIN);
+        assertNoGap(Timestamp.MIN,  Timestamp.MAX);
+        assertNoGap(Timestamp.MIN,  Timestamp.MIN);
+        assertNoGap(Timestamp.MAX,  Timestamp.MAX);
+        assertNoGap(Timestamp.now(),Timestamp.now());
+        assertNoGap(Timestamp.of(0,0,0,0),Timestamp.of(0,0,59,0));
+    }
+
+    @Test
+    public void gap_when_times_are_too_far_apart() {
+        assertGap(Timestamp.MIDNIGHT, Timestamp.NOON);
+        assertGap(Timestamp.NOON,     Timestamp.MIDNIGHT);
+        assertGap(Timestamp.of(0,0,0,0),Timestamp.of(0,1,0,0));
+        assertGap(Timestamp.of(0,0,0,0),Timestamp.of(1,0,0,0));
+    }
+
+    @Test
+    public void gap_when_either_time_is_null() {
+        assertGap(null, Timestamp.NOON);
+        assertGap(Timestamp.MIDNIGHT,null);
+    }
+
+    void assertNoGap(Timestamp t1, Timestamp t2) {
+        assertFalse(detector.isGapBetween(t1,t2));
+    }
+
+    void assertGap(Timestamp t1, Timestamp t2) {
+        assert(detector.isGapBetween(t1,t2));
+    }
+
+}
